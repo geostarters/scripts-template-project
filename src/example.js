7922cf364b84b8c425f931e8d53c2d0796ef2a89
Jess <jessisena@gmail.com>
2019-03-25 10:27:39 +0100

    add geostarters/common example of use

--- src/example.js
+++ src/example.js
@@ -1,10 +1,15 @@
 // @flow
 "use strict";
 
+
+//How to use @geostarters/common library (node version)
+const { Utils } = require("@geostarters/common/dist/node");
+
+//How to use flowtyped definition
 const myPoint: Point2d = {
 	x: 1,
 	y: 2
 };
 
-
 console.log(myPoint.x, myPoint.y);
+console.log(Utils.invertColor("#FFFFFF"));
