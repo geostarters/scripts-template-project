0ea88aae2bcb68fe63bd56cea401a1fcf801e70e
bolo <bolosig@gmail.com>
2019-10-24 14:32:21 +0200

    agregar prueba de test con jest

---
+++ src/point.js
@@ -0,0 +1,19 @@
+// @flow
+"use strict";
+
+
+class Point {
+
+	lat: number;
+	lon: number;
+
+
+	static createPoint(point: Point2d) {
+		
+		return {lat: point.y, lon:  point.x};
+	
+	}
+
+}
+
+module.exports = Point;
