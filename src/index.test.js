6e6a26fc88a58ff9391150b55e782e87beba4cf2
Wladimir Szczerban <bolosig@gmail.com>
2020-09-10 10:27:42 +0200

    add test

---
+++ src/index.test.js
@@ -0,0 +1,20 @@
+// @flow
+"use strict";
+
+const Point = require("./index");
+
+describe("InstamapsService library: ", () => {
+
+	const myPoint: Point2d = {
+		x: 1,
+		y: 2
+	};
+
+	it("create point {lat: 2, lon: 1}", () => {
+
+		const result = {"lat": 2, "lon": 1};
+		expect(Point.createPoint(myPoint)).toEqual(result);
+
+	});
+
+});
